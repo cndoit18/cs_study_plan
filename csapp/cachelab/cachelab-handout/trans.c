93c4daec4a85bc582f894b95c934a3033a59c5cd
cndoit18 <cndoit18@outlook.com>
2021-08-12 23:34:23 +0800

    update

--- csapp/cachelab/cachelab-handout/trans.c
+++ csapp/cachelab/cachelab-handout/trans.c
@@ -22,6 +22,70 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    if (M == 32)
+    {
+        int v1, v2, v3, v4, v5, v6, v7, v8;
+        for (int n = 0; n < N; n += 8)
+        {
+            for (int m = 0; m < M; m += 8)
+            {
+                for (int k = n; k < n + 8; k++)
+                {
+                    v1 = A[k][m];
+                    v2 = A[k][m + 1];
+                    v3 = A[k][m + 2];
+                    v4 = A[k][m + 3];
+                    v5 = A[k][m + 4];
+                    v6 = A[k][m + 5];
+                    v7 = A[k][m + 6];
+                    v8 = A[k][m + 7];
+                    B[m][k] = v1;
+                    B[m + 1][k] = v2;
+                    B[m + 2][k] = v3;
+                    B[m + 3][k] = v4;
+                    B[m + 4][k] = v5;
+                    B[m + 5][k] = v6;
+                    B[m + 6][k] = v7;
+                    B[m + 7][k] = v8;
+                }
+            }
+        }
+    }
+
+    if (M == 64)
+    {
+        int v1, v2, v3, v4;
+        for (int n = 0; n < N; n += 4)
+        {
+            for (int m = 0; m < M; m += 4)
+            {
+                for (int k = n; k < n + 4; k++)
+                {
+                    v1 = A[k][m];
+                    v2 = A[k][m + 1];
+                    v3 = A[k][m + 2];
+                    v4 = A[k][m + 3];
+                    B[m][k] = v1;
+                    B[m + 1][k] = v2;
+                    B[m + 2][k] = v3;
+                    B[m + 3][k] = v4;
+                }
+            }
+        }
+    }
+    else
+    {
+        int i, j, tmp;
+
+        for (i = 0; i < N; i++)
+        {
+            for (j = 0; j < M; j++)
+            {
+                tmp = A[i][j];
+                B[j][i] = tmp;
+            }
+        }
+    }
 }
 
 /* 
