78074064f9af0128a909d8ea26d05f4c6c660611
cndoit18 <cndoit18@outlook.com>
2022-01-07 11:48:40 +0800

    add leetcode 232

---
+++ leetcode/stack/232.go
@@ -0,0 +1,58 @@
+package stack
+
+import (
+	"container/list"
+)
+
+type MyQueue struct {
+	inputStack  *list.List
+	outputStack *list.List
+}
+
+func Constructor() MyQueue {
+	return MyQueue{
+		inputStack:  list.New(),
+		outputStack: list.New(),
+	}
+}
+
+func (this *MyQueue) Push(x int) {
+	this.inputStack.PushFront(x)
+}
+
+func (this *MyQueue) Pop() int {
+	if this.outputStack.Len() == 0 {
+		for current := this.inputStack.Front(); current != nil; {
+			next := current.Next()
+			this.outputStack.PushFront(this.inputStack.Remove(current))
+			current = next
+		}
+	}
+
+	return this.outputStack.Remove(this.outputStack.Front()).(int)
+}
+
+func (this *MyQueue) Peek() int {
+	if this.outputStack.Len() == 0 {
+		for current := this.inputStack.Front(); current != nil; {
+			next := current.Next()
+			this.outputStack.PushFront(this.inputStack.Remove(current))
+			current = next
+		}
+	}
+
+	return this.outputStack.Front().Value.(int)
+}
+
+func (this *MyQueue) Empty() bool {
+	return this.inputStack.Len() == 0 && this.outputStack.Len() == 0
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * param_2 := obj.Pop();
+ * param_3 := obj.Peek();
+ * param_4 := obj.Empty();
+ */
