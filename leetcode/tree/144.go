7f12fbc105b5df298800a0b3dc5e56a063832fa8
cndoit18 <cndoit18@outlook.com>
2022-01-12 22:19:23 +0800

    add leetcode 144

---
+++ leetcode/tree/144.go
@@ -0,0 +1,13 @@
+package tree
+
+func preorderTraversal(root *TreeNode) []int {
+	result := []int{}
+	if root == nil {
+		return result
+	}
+	result = append(result, root.Val)
+	result = append(result, preorderTraversal(root.Left)...)
+	result = append(result, preorderTraversal(root.Right)...)
+
+	return result
+}
