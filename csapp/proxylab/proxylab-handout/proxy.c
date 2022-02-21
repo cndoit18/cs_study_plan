a9ce3d56d64c7f1d33ae3310e3efc58109df9b79
cndoit18 <cndoit18@outlook.com>
2022-02-21 14:01:19 +0800

    add cache

--- csapp/proxylab/proxylab-handout/proxy.c
+++ csapp/proxylab/proxylab-handout/proxy.c
@@ -6,12 +6,35 @@ void doit(int fd);
 void parse_uri(char* uri, char* hostname, char* port, char* path);
 void* thread(void* vargp);
 
+struct entry {
+    char* key;
+    char* value;
+};
+
+struct hashmap {
+    uint64_t (*hashcode)(const char* key);
+    size_t length;
+    size_t capactiy;
+    struct entry* data;
+    pthread_mutex_t lock;
+};
+
+static uint64_t hashcode(const char* key);
+void map_init(struct hashmap* map);
+char* map_get(struct hashmap* map, const char* key);
+void map_set(struct hashmap* map, const char* key, const char* value);
+void map_destroy(struct hashmap* map);
+
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
 
+#define FNV_OFFSET 14695981039346656037UL
+#define FNV_PRIME 1099511628211UL
+
 /* You won't lose style points for including this long line in your code */
 static const char* user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
+static struct hashmap cache;
 
 int main(int argc, char* argv[])
 {
@@ -27,7 +50,7 @@ int main(int argc, char* argv[])
     }
 
     listenfd = Open_listenfd(argv[1]);
-
+    map_init(&cache);
     while (1) {
         clientlen = sizeof(clientaddr);
         connfdp = Malloc(sizeof(int));
@@ -37,7 +60,7 @@ int main(int argc, char* argv[])
 
         Pthread_create(&tid, NULL, thread, connfdp);
     }
-
+    map_destroy(&cache);
     return 0;
 }
 
@@ -57,6 +80,7 @@ void doit(int fd)
 {
     char buf[MAXLINE], method[MAXLINE], version[MAXLINE], uri[MAXLINE];
     char hostname[MAXLINE], port[MAXLINE], path[MAXLINE];
+    char *buffer;
     rio_t clientrio, servrio;
     int servfd, n;
 
@@ -72,7 +96,10 @@ void doit(int fd)
             "Proxy does not implement this method");
         return;
     }
-
+    if ((buffer = map_get(&cache, uri)) != NULL) {
+        Rio_writen(fd, buffer, strlen(buffer));
+        return;
+    }
     parse_uri(uri, hostname, port, path);
 
     // proxy
@@ -88,11 +115,16 @@ void doit(int fd)
     sprintf(buf, "Proxy-Connection: close\r\n\r\n");
     Rio_writen(servfd, buf, strlen(buf));
 
+    buffer = Malloc(MAX_OBJECT_SIZE);
+    memset(buffer, '\0', MAX_OBJECT_SIZE);
     Rio_readinitb(&servrio, servfd);
     while ((n = Rio_readlineb(&servrio, buf, MAXLINE)) != 0) { // line:netp:readhdrs:checkterm
+        strncat(buffer, buf, n);
         Rio_writen(fd, buf, n);
     }
-
+    map_set(&cache, uri, buffer);
+    free(buffer);
+    
     Close(servfd);
 }
 
@@ -148,4 +180,109 @@ void parse_uri(char* uri, char* hostname, char* port, char* path)
             return;
         }
     }
+}
+
+static uint64_t hashcode(const char* key)
+{
+    uint64_t hash = FNV_OFFSET;
+    for (const char* p = key; *p; p++) {
+        hash ^= (uint64_t)(unsigned char)(*p);
+        hash *= FNV_PRIME;
+    }
+
+    return hash;
+}
+
+void map_init(struct hashmap* map)
+{
+    map->capactiy = 16;
+    map->length = 0;
+    map->hashcode = &hashcode;
+    map->data = Malloc(map->capactiy * sizeof(struct entry));
+    memset(map->data, 0, map->capactiy * sizeof(struct entry));
+    pthread_mutex_init(&map->lock, NULL);
+}
+
+char* map_get(struct hashmap* map, const char* key)
+{
+    if (map == NULL || map->hashcode == NULL || key == NULL)
+        return NULL;
+
+    pthread_mutex_lock(&map->lock);
+    uint64_t hash = (map->hashcode)(key);
+    size_t index = (size_t)hash & (map->capactiy - 1);
+
+    while (map->data[index].key != NULL) {
+        if (strcmp(key, map->data[index].key) == 0) {
+            pthread_mutex_unlock(&map->lock);
+            return map->data[index].value;
+        }
+
+        index++;
+        if (index >= map->capactiy) {
+            index = 0;
+        }
+    }
+
+    pthread_mutex_unlock(&map->lock);
+    return NULL;
+}
+
+void map_set(struct hashmap* map, const char* key, const char* value)
+{
+    if (value == NULL || key == NULL || map == NULL) {
+        return;
+    }
+
+    pthread_mutex_lock(&map->lock);
+    if (map->length >= map->capactiy / 2) {
+        size_t new_capactiy = map->capactiy * 2;
+        if (new_capactiy < map->capactiy) {
+            pthread_mutex_unlock(&map->lock);
+            return;
+        }
+
+        map->data = (struct entry*)Realloc(map->data, new_capactiy * sizeof(struct entry));
+        map->capactiy = new_capactiy;
+    }
+
+    uint64_t hash = (map->hashcode)(key);
+    size_t index = (size_t)hash & (map->capactiy - 1);
+
+    while (map->data[index].key != NULL) {
+        if (strcmp(key, map->data[index].key) == 0) {
+            free(map->data[index].value);
+            map->data[index].value = strdup(value);
+            pthread_mutex_unlock(&map->lock);
+            return;
+        }
+
+        index++;
+        if (index >= map->capactiy) {
+            index = 0;
+        }
+    }
+
+    map->length++;
+    map->data[index].key = strdup(key);
+    map->data[index].value = strdup(value);
+    pthread_mutex_unlock(&map->lock);
+}
+
+void map_destroy(struct hashmap* map)
+{
+    if (map == NULL)
+        return;
+    pthread_mutex_lock(&map->lock);
+    for (int i = 0; i < map->capactiy && map->data[i].key != NULL; i++) {
+        free(map->data[i].key);
+        free(map->data[i].value);
+    }
+
+    free(map->data);
+    map->capactiy = 0;
+    map->data = NULL;
+    map->length = 0;
+    pthread_mutex_unlock(&map->lock);
+    pthread_mutex_destroy(&map->lock);
 }
