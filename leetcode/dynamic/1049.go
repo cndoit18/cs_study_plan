36fe958baca35cc2866c6f3138b3c148eaba42a2
cndoit18 <cndoit18@outlook.com>
2022-06-10 01:36:52 +0800

    add leetcode 1049

---
+++ leetcode/dynamic/1049.go
@@ -0,0 +1,24 @@
+package dynamic
+
+func lastStoneWeightII(stones []int) int {
+	weight := 0
+	for _, w := range sÏtones {
+		weight += w
+	}
+
+	max := func(x, y int) int {
+		if x > y {
+			return x
+		}
+		return y
+	}
+
+	target := int(weight / 2)
+	dp := make([]int, weight)
+	for _, w := range stones {
+		for j := target; j >= w; j-- {
+			dp[j] = max(dp[j], dp[j-w]+w)
+		}
+	}
+	return weight - (dp[target] << 1)
+}
