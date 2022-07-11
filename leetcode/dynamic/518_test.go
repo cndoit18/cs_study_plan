be1f27a0e439d70609fdcfebce6b4a7343315b74
cndoit18 <cndoit18@outlook.com>
2022-07-11 22:44:08 +0800

    add leetcode 518

---
+++ leetcode/dynamic/518_test.go
@@ -0,0 +1,25 @@
+package dynamic
+
+import "testing"
+
+func Test_change(t *testing.T) {
+	type args struct {
+		amount int
+		coins  []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"case 1", args{3, []int{2}}, 0},
+		{"case 2", args{5, []int{1, 2, 5}}, 4},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := change(tt.args.amount, tt.args.coins); got != tt.want {
+				t.Errorf("change() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
