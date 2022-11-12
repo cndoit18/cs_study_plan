607c7b97f78fe6c634a09c963244e432471ed48e
cndoit18 <cndoit18@outlook.com>
2022-11-13 03:29:24 +0800

    finish part4

--- frontend/fullstack/part4/controllers/notes.js
+++ frontend/fullstack/part4/controllers/notes.js
@@ -1,6 +1,16 @@
+const jwt = require('jsonwebtoken')
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+    const authorization = request.header('Authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+    return null
+}
+
 
 notesRouter.get('/', async (request, response) => {
     response.json(await Note.find({}).populate('user', { username: 1, name: 1 }))
@@ -17,6 +27,11 @@ notesRouter.get('/:id', async (request, response, next) => {
 
 notesRouter.post('/', async (request, response, next) => {
     const body = request.body
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
 
     const user = await User.findById(body.userId)
 
--- frontend/fullstack/part4/utils/middleware.js
+++ frontend/fullstack/part4/utils/middleware.js
@@ -13,13 +13,25 @@ const unknownEndpoint = (request, response) => {
 }
 
 const errorHandler = (error, request, response, next) => {
-    logger.error(error.message)
-
     if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
+        return response.status(400).send({
+            error: 'malformatted id'
+        })
     } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
+        return response.status(400).json({
+            error: error.message
+        })
+    } else if (error.name === 'JsonWebTokenError') {
+        return response.status(401).json({
+            error: 'invalid token'
+        })
+    } else if (error.name === 'TokenExpiredError') {
+        return response.status(401).json({
+            error: 'token expired'
+        })
     }
+
+    logger.error(error.message)
 
     next(error)
 }
