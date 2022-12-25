48322216b8990f66ed4be07c349cccdf0ead0e03
cndoit18 <cndoit18@outlook.com>
2022-12-26 01:55:06 +0800

    add c++ primer

---
+++ c++/chapter01/1_4.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+	int x = 2, y = 3;
+	std::cout << x + y << "\n" << x * y << std::endl;
+	return 0;
+}
---
+++ c++/chapter01/1_9.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	int i = 51, sum = 50;
+	while(i <= 100) {
+		sum += i;
+		i ++;
+	}
+	std::cout << sum << std::endl;
+	return 0;
+}
