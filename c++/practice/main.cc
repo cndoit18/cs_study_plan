22b36c2b3d46bc0a91b819d7a0ec9a103922354f
cndoit18 <cndoit18@outlook.com>
2022-12-31 01:10:07 +0800

    update oop

--- c++/practice/main.cc
+++ c++/practice/main.cc
@@ -1,11 +1,49 @@
 #include "validate.h"
 #include <iostream>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
+inline void default_func(int x = 10) { std::cout << x << std::endl; }
+
+// const与重载
+void fcn(int *value) { return; }
+void fcn(const int *value) { return; }
+// void fcn(int *const value) { return; } error
+
+// TODO: 没有看懂constexpr的逻辑
+constexpr int constexpr_func(int x) { return x + 3; }
+
 using std::string;
 
-int main() {
+// struct OOP
+struct Sales_data {
+  Sales_data() = default;
+  // Sales_data() {}
+
+  Sales_data(std::string bookNo) { this->bookNo = bookNo; }
+  // this 是一个指针
+  std::string isbn() { return this->bookNo; }
+  std::string bookNo;
+};
+
+// class OOP
+class C_Sales_data {
+public:
+  C_Sales_data() = default;
+  C_Sales_data(std::string bookNo) { this->bookNo = bookNo; }
+  std::string isbn() { return this->bookNo; }
+
+private:
+  std::string bookNo;
+};
+
+int main(int argc, char **argv) {
+
+  for (decltype(argc) index = 0; index < argc; index++) {
+    std::cout << "args[" << index << "]:" << argv[index] << std::endl;
+  }
+
   // initialization
   int x = 1;
 
@@ -56,7 +94,15 @@ int main() {
   // ptr_c = ptr; error
   // *ptr_c = 20; no error
 
+  // int c = 10; error
+  // 需要有固定地址，如果是int c将放在栈中
+  static int c = 10;
+  constexpr int *const ptr_cc = &c;
+
   string line;
+
+  // int q = 10;
+  decltype(i) q = 10;
 
   while (getline(std::cin, line)) {
     std::cout << "Echo: " << line << std::endl;
@@ -69,6 +115,45 @@ int main() {
   for (auto index = vectors.begin(); index != vectors.end(); ++index) {
     std::cout << *index << std::endl;
   }
+
+  int arrays[] = {1, 2, 3};
+  std::cout << "arrays size is :" << sizeof(arrays) / sizeof(arrays[0])
+            << std::endl;
+
+  // 强制转换
+  // static_cast
+  // const_cast
+  // reinterpret_cast 提供较低层次上的重新解释
+  int n = 5;
+  void *vptr = &n;
+  auto sc = static_cast<int *>(vptr);
+
+  // 异常处理
+
+  try {
+    throw std::exception();
+  } catch (std::exception) {
+    std::cerr << "catch an exception" << std::endl;
+  }
+
+  default_func();
+
+  constexpr_func(3);
+  int val = 1;
+  while (val < 10) {
+    constexpr_func(++val);
+  }
+
+  assert(1 == 1);
+
+  Sales_data sd = Sales_data("test");
+  // 没有权限隔离
+  std::cout << sd.isbn() << sd.bookNo << std::endl;
+
+  Sales_data empty_sd = Sales_data();
+
+  C_Sales_data cd = C_Sales_data("c_test");
+  // std::cout <<  cd.bookNo << std::endl; error bookNo 是私有属性，无法被访问
 
   string str = "Hello, World!";
   std::cout << str << std::endl;
--- c++/practice/validate.h
+++ c++/practice/validate.h
@@ -1,10 +1,11 @@
 #ifndef VALIDATE_HH
 #define VALIDATE_HH
-
 int y = 2;
 
 // 需要指定extern吗?
 // extern const int z = 3;
 const int z = 3;
+
+#define NOEDEBUG
 
 #endif
