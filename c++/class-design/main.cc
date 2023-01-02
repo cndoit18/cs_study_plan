76c18b0a881d1e1705945fbbddc630367be32f12
cndoit18 <cndoit18@outlook.com>
2023-01-02 18:52:10 +0800

    update c++

--- c++/class-design/main.cc
+++ c++/class-design/main.cc
@@ -1,12 +1,15 @@
 #include <iostream>
 
 using namespace std;
-template <typename _T> bool compare(const _T &x, const _T &y) { return x == y; }
+
+template <typename _T> inline bool compare(const _T &x, const _T &y) {
+  return x == y;
+}
 
 class Sales_data {
 public:
   Sales_data() = default;
-  Sales_data(string bookNo);
+  Sales_data(string bookNo) = delete;
   // Sales_data(const Sales_data &clone) = default;
   // Sales_data(Sales_data &&clone) { cout << "right" << endl; }
 
@@ -22,6 +25,7 @@ public:
     return *this;
   }
   virtual string Name() { return "Sales_data"; }
+  template <typename T> string Other() { return "Other"; }
 
 protected:
   string name = "protecetd";
@@ -30,12 +34,19 @@ private:
   string bookNo;
 };
 
-template <typename T> class Quota final : public Sales_data {
+// typename/class
+template <typename T, typename F = int> class Quota final : public Sales_data {
 public:
   virtual string Name() { return "Quota"; }
+  // virtual void useTemplate() { shared_ptr<T> data; }
+  virtual void useTemplate(typename T::quota_type data) {}
 };
 
-Sales_data::Sales_data(string bookNo) : bookNo(bookNo) {}
+class QuotaType {
+public:
+  // typedef int quota_type;
+  using quota_type = int;
+};
 
 Sales_data::~Sales_data() { cout << "free" << endl; }
 
@@ -43,6 +54,8 @@ string Sales_data::isbn() const {
   // this->bookNo = "123"; error
   return this->bookNo;
 }
+
+auto fcn() { return 1 + 1; }
 
 int main(int argc, char **args) {
   compare(1, 2);
@@ -54,12 +67,14 @@ int main(int argc, char **args) {
 
   Sales_data left = clone;
   Sales_data right = std::move(clone);
+  // Sales_data d("test"); error 字符串构造器被拒绝
   sd.release();
   // cout << clone.ptr << endl;
 
-  Sales_data *q = new Quota<int>();
+  Sales_data *q = new Quota<QuotaType>();
   // 回避虚函数
   cout << q->Sales_data::Name() << endl;
+  q->Other<int>();
 
   return 0;
 }
